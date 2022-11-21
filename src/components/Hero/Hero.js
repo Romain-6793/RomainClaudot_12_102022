c2ba542d7b06fe77492b6640b90f18f006170867
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2022-11-21 11:33:12 +0100

    Before fetching multiple URLS

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,11 +14,26 @@ function Hero(props) {
     //Ici on va gérer l'affichage conditionnel des données que l'on va faire remonter en props.
 
     const data = props.data
+
+    // Comment me passer de ces variables ?
+
+    let firstName = useRef(data.USER_MAIN_DATA[0].userInfos.firstName)
+    let keyData = useRef(data.USER_MAIN_DATA[0].keyData)
+    let score = useRef(data.USER_MAIN_DATA[0].todayScore)
+    let activitySessions = useRef(data.USER_ACTIVITY[0].sessions)
+    let averageSessions = useRef(data.USER_AVERAGE_SESSIONS[0].sessions)
+    let skillData = useRef(data.USER_PERFORMANCE[0].data)
+    let skillKind = useRef(data.USER_PERFORMANCE[0].kind)
     const BASE_URL = `http://localhost:3000`
     const { id } = useParams()
     // eslint-disable-next-line no-unused-vars
     const [response, loading, hasError] = useFetch(`${BASE_URL}/user/${id}`)
+    // || (`${BASE_URL}/user/${id}/activity`)
+    // || (`${BASE_URL}/user/${id}/average-sessions`)
+    // || (`${BASE_URL}/user/${id}/performance`)
     console.log(response)
+    // firstName.current = response.data.userInfos.firstName
+    // console.log(firstName.current)
 
 
     function useFetch(url, opts) {
@@ -37,6 +52,13 @@ function Hero(props) {
                     setResponse(res)
                     console.log(res)
                     setLoading(false)
+                    firstName.current = res.data.userInfos.firstName
+                    // activitySessions.current = activity[0].sessions
+                    // averageSessions.current = avSessions[0].sessions
+                    // skillData.current = skills[0].data
+                    // skillKind.current = skills[0].kind
+                    score.current = res.data.todayScore || res.data.score
+                    keyData.current = res.data.keyData
                 }
                 )
                 .catch(err => {
@@ -93,22 +115,8 @@ function Hero(props) {
     // }, [id])
 
 
-    // Ou bien mettre un objet vide dans le useState ou data.USER_MAIN_DATA[0]. Tout dépend du choix.
-    // Il faudra peut-être supprimer le DD.
-    let firstName = useRef(data.USER_MAIN_DATA[0].userInfos.firstName)
-    let keyData = useRef(data.USER_MAIN_DATA[0].keyData)
-    let score = useRef(data.USER_MAIN_DATA[0].todayScore)
-    // || useRef(data.USER_MAIN_DATA[0].score)
-    // eslint-disable-next-line no-unused-vars
-    const [basicActivity, setActivity] = useState(data.USER_ACTIVITY[0])
-    let activitySessions = useRef(data.USER_ACTIVITY[0].sessions)
-    // eslint-disable-next-line no-unused-vars
-    const [basicSessions, setSessions] = useState(data.USER_AVERAGE_SESSIONS[0])
-    let averageSessions = useRef(data.USER_AVERAGE_SESSIONS[0].sessions)
-    // eslint-disable-next-line no-unused-vars
-    const [basicSkills, setSkills] = useState(data.USER_PERFORMANCE[0])
-    let skillData = useRef(data.USER_PERFORMANCE[0].data)
-    let skillKind = useRef(data.USER_PERFORMANCE[0].kind)
+
+
 
 
 
